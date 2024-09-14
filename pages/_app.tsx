0cd2097178b913546ec1181ee2a42ef585493086
ysadamt <adam.teo321@gmail.com>
2024-09-14 17:09:33 -0400

    convex clerk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(
     process.env.NEXT_PUBLIC_CONVEX_URL as string
@@ -8,8 +10,10 @@ const convex = new ConvexReactClient(
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ConvexProvider client={convex}>
-            <Component {...pageProps} />
-        </ConvexProvider>
+        <ClerkProvider publishableKey="pk_test_YnJpZWYtcG9zc3VtLTU0LmNsZXJrLmFjY291bnRzLmRldiQ">
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <Component {...pageProps} />
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
     );
 }
