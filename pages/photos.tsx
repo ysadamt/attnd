7a2ef24f0364b79f5642b8d647b27b3386f3d5b7
rita-hg <rhguerr@tamu.edu>
2024-09-14 15:16:44 -0500

    moved photos to public

--- pages/photos.tsx
+++ pages/photos.tsx
@@ -8,47 +8,47 @@ export interface Photo {
 
 const photos: Photo[] = [
   {
-    src: "./photos/group1.jpg",
+    src: "../public/photos/group1.jpg",
     width: 800,
     height: 600,
   },
   {
-    src: "./photos/group2.jpg",
+    src: "../public/photos/group2.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group3.jpg",
+    src: "../public/photos/group3.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group4.jpg",
+    src: "../public/photos/group4.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group5.jpg",
+    src: "../public/photos/group5.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group6.jpg",
+    src: "../public/photos/group6.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group7.jpg",
+    src: "../public/photos/group7.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group8.jpg",
+    src: "../public/photos/group8.jpg",
     width: 1200,
     height: 800,
   },
   {
-    src: "./photos/group9.jpg",
+    src: "../public/photos/group9.jpg",
     width: 1200,
     height: 800,
   },
