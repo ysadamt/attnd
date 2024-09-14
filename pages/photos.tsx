8b65a01a1d9596632e693714b82f7770e93e8513
harshitha <harshitham1203@gmail.com>
2024-09-14 15:53:04 -0400

    photo gallery

---
+++ pages/photos.tsx
@@ -0,0 +1,57 @@
+// photos.ts
+
+export interface Photo {
+  src: string;
+  width: number;
+  height: number;
+}
+
+const photos: Photo[] = [
+  {
+    src: "./photos/group1.jpg",
+    width: 800,
+    height: 600,
+  },
+  {
+    src: "./photos/group2.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group3.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group4.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group5.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group6.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group7.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group8.jpg",
+    width: 1200,
+    height: 800,
+  },
+  {
+    src: "./photos/group9.jpg",
+    width: 1200,
+    height: 800,
+  },
+];
+
+export default photos;
