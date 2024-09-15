4e8362a758fedbdbb91c2f486fd829dd9a11c0a6
ysadamt <adam.teo321@gmail.com>
2024-09-15 14:48:50 -0400

    deploy application

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,16 +1,16 @@
-import { mutation } from "./_generated/server";
+// import { mutation } from "./_generated/server";
 
-export const generateUploadUrl = mutation(async (ctx) => {
-  return await ctx.storage.generateUploadUrl();
-});
+// export const generateUploadUrl = mutation(async (ctx) => {
+//   return await ctx.storage.generateUploadUrl();
+// });
 
-export const sendImage = mutation({
-    args: { storageId: v.id("_storage"), author: v.string() },
-    handler: async (ctx, args) => {
-      await ctx.db.insert("messages", {
-        body: args.storageId,
-        author: args.author,
-        format: "image",
-      });
-    },
-  });
+// export const sendImage = mutation({
+//     args: { storageId: v.id("_storage"), author: v.string() },
+//     handler: async (ctx, args) => {
+//       await ctx.db.insert("messages", {
+//         body: args.storageId,
+//         author: args.author,
+//         format: "image",
+//       });
+//     },
+//   });
